c9d1631c5158228affae9bde7627b26ba9f07852
Ki Won <kyun4525@gmail.com>
2024-04-05 15:28:38 +0900

    Time: 85 ms (14.97%), Space: 58.6 MB (85.78%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices) {
+    if (prices.length === 0) return 0;
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } 
+        
+        if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+};
