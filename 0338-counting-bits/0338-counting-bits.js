08fef391bab4dd555df03a0989f03732642b22c6
Ki Won <kyun4525@gmail.com>
2024-04-25 20:10:58 +0900

    Time: 92 ms (21.57%), Space: 56 MB (60.39%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,15 +3,25 @@
  * @return {number[]}
  */
 const countBits = function(n) {
-    const resultList = [];
-    let counts = 0;
+    const resultList = new Array(n + 1).fill(0);
 
-    while (counts <= n) {
-        const binaryN = counts.toString(2);
-
-        resultList.push(binaryN.split("1").length - 1);
-        counts++;
+    for (let i = 0; i <= n; i++) {
+        resultList[i] = countOnes(i);
     }
 
     return resultList;
 };
+
+function countOnes(num) {
+    let count = 0;
+    
+    while (num > 0) {
+        if (num % 2 === 1) {
+            count++;
+        }
+
+        num = Math.floor(num / 2);
+    }
+
+    return count;
+}
