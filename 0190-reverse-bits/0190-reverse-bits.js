de6b1335eb76a960fa1dd7947a1383c710a55d8c
Ki Won <kyun4525@gmail.com>
2024-04-27 15:49:20 +0900

    Time: 49 ms (94.47%), Space: 50.1 MB (96.42%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.js
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -3,14 +3,14 @@
  * @return {number} - a positive integer
  */
 const reverseBits = function(n) {
-    let resultDecimal = 0;
+    let decimalResult = 0;
     const reversedBits = n.toString(2).padStart(32, "0").split("").reverse().join("");
     
     for (let i = reversedBits.length - 1; i >= 0; i--) {
         if (reversedBits[i] === "1") {
-            resultDecimal += 2 ** (reversedBits.length - 1 - i);
+            decimalResult += 2 ** (reversedBits.length - 1 - i);
         }
     }
 
-    return resultDecimal;
+    return decimalResult;
 };
