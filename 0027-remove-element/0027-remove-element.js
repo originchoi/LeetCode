5cfad4f8cf666b2e4d1d06d707a96fbacf6be7e2
Ki Won <kyun4525@gmail.com>
2024-04-28 23:47:58 +0900

    Time: 57 ms (33.86%), Space: 48.8 MB (70.54%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function(nums, val) {
+    let result = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[result] = nums[i];
+            result++;
+        }
+    }
+
+    return result;
+};
