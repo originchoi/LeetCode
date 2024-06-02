0c0b32bfa69c1c6fbe6687509fe8c3e7005d196e
Ki Won <kyun4525@gmail.com>
2024-06-02 23:33:36 +0900

    Time: 37 ms (99.52%), Space: 48.4 MB (86.32%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        digits[i]++;
+        
+        if (digits[i] < 10) return digits;
+        
+        digits[i] = 0;
+    }
+    
+    digits.unshift(1);
+    
+    return digits;
+};
